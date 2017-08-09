7adb5b9afd57c31ed48040315b7b022f2686397d
Nathan Hare <n.hare@hotmail.com>
2017-08-09 17:40:22 -0400

    Add connect to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends React.Component {
 
   render() {
+    console.log('Props:', this.props)
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello </h1>
       </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps (calendar) {
+ const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+ return {
+   calendar: dayOrder.map((day) => ({
+     day,
+     meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+       meals[meal] = calendar[day][meal]
+         ? calendar[day][meal]
+         : null
+
+       return meals
+     }, {})
+   })),
+ }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps)(App);
