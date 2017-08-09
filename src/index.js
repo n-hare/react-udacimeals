e3e5707aa8855186de4230f855d88ae2848e0afa
Nathan Hare <n.hare@hotmail.com>
2017-08-09 00:56:03 -0400

    Add react-redux Provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import reducer from './reducers'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 const store = createStore(
   reducer,
@@ -13,7 +14,9 @@ const store = createStore(
 
 
 ReactDOM.render(
-    <App store={store} />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 registerServiceWorker()
